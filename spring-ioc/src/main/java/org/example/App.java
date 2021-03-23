35b46b611826cb6030261b26f3fbe4b8837f71d2
gqx <LuckyMr_Gqx@163.com>
2021-03-23 21:38:50 +0800

    properties配置读取

--- spring-ioc/src/main/java/org/example/App.java
+++ spring-ioc/src/main/java/org/example/App.java
@@ -29,12 +29,12 @@ public class App
         *   第一种方法：执行下面这局代码
         *   第二种方法：在xml文件中加入<context:component-scan base-package="org.example" />，扫描这个包下的所有类，然后再加载xml类(new ClassPathXmlApplicationContext("applicationTest.xml");)
         * */
-        ApplicationContext context1 = new AnnotationConfigApplicationContext("org.example");
+        /*ApplicationContext context1 = new AnnotationConfigApplicationContext("org.example");
         ITestService testServiceImpl = context1.getBean("testServiceImpl", ITestService.class); //注解不用写ID,所有ID默认为类名(首字母小写)
-        testServiceImpl.Query();
+        testServiceImpl.Query();*/
 
 
-        PropertiesDemo propertiesDemo = context1.getBean("propertiesDemo", PropertiesDemo.class);
+        PropertiesDemo propertiesDemo = context.getBean("propertiesDemo", PropertiesDemo.class);
         propertiesDemo.Do();
 
     }
